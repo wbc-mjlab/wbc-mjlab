8f3fc8441c41de7659c1c13cd237d5bbcf8586b4
simeon <simkaned@gmail.com>
2026-06-30 19:32:28 +0300

    clean up

--- src/wbc_mjlab/presets/se_actor.py
+++ src/wbc_mjlab/presets/se_actor.py
@@ -37,7 +37,7 @@ _SE_ADDED_TERMS: dict[str, ObservationTermCfg] = {
   "base_lin_vel": ObservationTermCfg(
     func=mdp.builtin_sensor,
     params={"sensor_name": ""},
-    noise=Unoise(n_min=-0.25, n_max=0.25),
+    noise=Unoise(n_min=-0.2, n_max=0.2),
   ),
 }
 
